d7b3c6bfcfb9df3e1dee5f9a190a23139666eb49
43bp2 <43bp@mail.ru>
2022-10-07 18:00:02 +0300

    add all htasks

---
+++ HTask15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели: ");
+int noDofW = Convert.ToInt32(Console.ReadLine());
+bool cheakweek = noDofW <= 0 || noDofW > 7;
+if (cheakweek) Console.WriteLine("Дня недели под таким номером не существует");
+else 
+{
+    bool cheak = noDofW > 5;
+    if (cheak) Console.WriteLine($"{noDofW} -> да");
+    else Console.WriteLine($"{noDofW} -> нет");
+}
