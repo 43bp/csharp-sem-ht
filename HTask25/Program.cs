69f08ae3df399e4dd8e8b67fabcafc793793786d
43bp2 <43bp@mail.ru>
2022-10-13 21:56:46 +0300

    add htasks sem4

---
+++ HTask25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральную степень, в которую это число возвести: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+degree = Natural (degree); // вызываем метод проверки числа на натуральность
+
+int result = Degree(number, degree); // вызываем метод возведения числа в степень
+Console.WriteLine($"Число {number} в степени {degree} = {result}");
+
+
+// ==================================================================================
+// Метод проверки числа на натуральность
+int Natural (int num)
+{   while (num <= 0)
+    {
+        Console.WriteLine("Это значение степени не натуральное!");
+        Console.WriteLine();
+        Console.WriteLine("Введите натуральную степень, в которую это число возвести: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+
+// ==================================================================================
+// Метод возведения числа в степень
+int Degree (int num, int deg)
+{
+    int multipl = num;
+    for (int i = 1; i < deg; i++)
+    {
+        checked
+        {
+            multipl = multipl * num;
+        }
+    }
+    return multipl;
+}
---
+++ HTask27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigits(number); // вызываем метод суммирования цифр в числе
+Console.WriteLine($"Сумма цифр в числе {number} = {result}");
+
+
+//====================================================
+// Метод суммирования цифр в числе
+int SumDigits (int num)
+{
+    int digit1 = default;
+    int digit2 = default;
+    int sumDigits = default;
+    while (num > 0)
+    {
+        digit1 = num % 10;
+        num = num / 10;
+        digit2 = num % 10;
+        sumDigits = sumDigits + digit1 + digit2;
+        num = num / 10;
+    }
+    return sumDigits;
+}
